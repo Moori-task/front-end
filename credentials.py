6898823dbc1c261b3ea0724d7b89e9832cf74754
sepehr kianian <sepehr kianian>
2024-01-16 17:20:30 +0330

    fix: init web_hook

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6484830100:AAG9itC_ZgVbLvmAVvK6oVb3HLi8tC4DRlE"
 bot_user_name = "palacefinder_bot"
-URL = "the heroku app link that we will create later"
+URL = "http://45.149.77.203:5000/"
--- move_to_server.sh
+++ move_to_server.sh
@@ -1,3 +1,4 @@
 docker build . -t moori
-docker save moori -o bot.tar
-scp bot.tar root@45.149.77.203:/home/sepehr
+docker run -d -p 5000:5000 moori
+# docker save moori -o bot.tar
+# scp bot.tar root@45.149.77.203:/home/sepehr
