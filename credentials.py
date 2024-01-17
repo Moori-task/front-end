06e9a13016059e16058947e85a6045248d9ed2d2
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-17 20:22:26 +0330

    chore: remove unused codes

--- credentials.py
+++ credentials.py
@@ -1,3 +1,2 @@
 bot_token = "6484830100:AAG9itC_ZgVbLvmAVvK6oVb3HLi8tC4DRlE"
 bot_user_name = "palacefinder_bot"
-URL = "http://45.149.77.203:5000/"
--- move_to_server.sh
+++ move_to_server.sh
@@ -1,4 +1,4 @@
 docker build . -t moori
-docker run -d -p 5000:5000 moori
+docker run -d moori
 # docker save moori -o bot.tar
 # scp bot.tar root@45.149.77.203:/home/sepehr
