7a3b02e35b2a4e41f8506509dfa42168a65189bc
sepehr kianian <sepehr kianian>
2024-01-17 20:12:12 +0330

    ff

---
+++ temp4.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from credentials import bot_token
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print("hello")
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+
+app = ApplicationBuilder().token(bot_token).build()
+
+app.add_handler(CommandHandler("/start", hello))
+
+app.run_polling()
