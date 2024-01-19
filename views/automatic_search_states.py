15c1492cccd9cfcb19ee2cccf84fc58ed5f15662
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-19 20:59:46 +0330

    feat(automatic_search): define current menu states using state abstracts

---
+++ views/automatic_search_states.py
@@ -0,0 +1,112 @@
+from debugger import Debugger
+from .automatic_search_abstracts import SingleTransitionState, StateTransition
+
+from typing import List, Type
+from telegram import Update, ReplyKeyboardRemove
+from telegram.ext import (
+    BaseHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+import pprint
+
+class CapacityState(SingleTransitionState):
+    async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(
+            "اقامتگاه شما باید چند نفر ظرفیت داشته‌باشد؟",
+        )
+
+    class Transition(StateTransition):
+        def next_state(self):
+            return RateState(self.controller)
+
+        async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+            self.controller.set_capacity(int(update.message.text))
+
+        def get_handler(self):
+            return MessageHandler(filters.Regex("^[1-9][0-9]*$"), self.handle)
+
+
+class RateState(SingleTransitionState):
+    async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(
+            "امتیاز اقامتگاه از چند به بالا باشد مناسب است؟"
+        )
+
+    class Transition(StateTransition):
+        def next_state(self):
+            return AreaMinState(self.controller)
+
+        async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+            self.controller.set_min_rate(int(update.message.text))
+
+        def get_handler(self):
+            # you should include 0 in rate, too
+            return MessageHandler(filters.Regex("^[1-9][0-9]*$"), self.handle)
+        
+class AreaMinState(SingleTransitionState):
+    async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("خانه حداقل چند متر باید باشد؟")
+
+    class Transition(StateTransition):
+        def next_state(self):
+            return AreaMaxState(self.controller)
+
+        async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+            self.controller.set_min_area_size(int(update.message.text))
+
+        def get_handler(self):
+            return MessageHandler(filters.Regex("^[1-9][0-9]*$"), self.handle)
+        
+class AreaMaxState(SingleTransitionState):
+    async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("خانه حداقل چند متر باید باشد؟")
+
+    class Transition(StateTransition):
+        def next_state(self):
+            return AreaMaxState(self.controller)
+
+        def get_handler(self):
+            return MessageHandler(filters.Regex("^[1-9][0-9]*$"), self.handle)
+
+        def make_pretty(self, item) -> str:
+            return pprint.pformat(item)
+        
+        async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+            self.controller.set_max_area_size(int(update.message.text))
+            places = self.controller.get_places()
+            await update.message.reply_text("خدمت شما!\n" + self.make_pretty(places))
+            return ConversationHandler.END
+        
+
+
+class StartTransition(StateTransition):
+    def get_handler(self):
+        return CommandHandler("start", self.handle)
+
+    async def run(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(
+            "در این دستور معیارهای جستجو را خود به شما عرضه می‌کنیم.\n"
+            "در هر زمان، برای لغو این عملیات از دستور /cancel استفاده کنید.\n"
+        )
+
+    def next_state(self):
+        return CapacityState(self.controller)
+    
+
+class CancelTransition(StateTransition):
+    def get_handler(self):
+        return CommandHandler("cancel", self.handle)
+
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        user = update.message.from_user
+        Debugger().get_logger().info(
+            "User %s canceled the conversation.", user.first_name
+        )
+        await update.message.reply_text(
+            "عملیات لغو شد", reply_markup=ReplyKeyboardRemove()
+        )
+        return ConversationHandler.END
